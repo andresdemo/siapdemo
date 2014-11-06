b2cfd32cb5be6559fafc15511ff830bdc4d04fdf
Francisco F <ferraro@siap.gob.mx>
2014-11-06 12:26:43 -0600

    Cambios en el tipo de lista

--- application/views/test_view.php
+++ application/views/test_view.php
@@ -30,15 +30,15 @@
 
   </head>
   <body>
-  	<ol>
+  	<ul>
     <?php foreach($rss as $item): ?>
-    		<li> <h3><?php echo $item['title']; ?> </h3></li>
-    		<li> <?php echo $item['description']; ?> </li>
-    		<li> <?php echo $item['author']; ?> </li>
-    		<li> <?php echo $item['pubDate']; ?> </li>
-    		<li> <?php echo $item['link']; ?> </li>
+    		<li>Nota: <h3><?php echo $item['title']; ?> </h3></li>
+    		<li>Desc:  <?php echo $item['description']; ?> </li>
+    		<li>Autor:  <?php echo $item['author']; ?> </li>
+    		<li>Fecha: <?php echo $item['pubDate']; ?> </li>
+    		<li>Liga:  <?php echo $item['link']; ?> </li>
     <?php endforeach; ?>
-	</ol>
+</ul>
     
   </body>
 </html>
